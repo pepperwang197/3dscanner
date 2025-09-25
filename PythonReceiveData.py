89104c593ae7decd4ae9173af8a507cfe336e5c4
Pepper <93462639+pepperwang197@users.noreply.github.com>
2025-09-24 23:59:00 -0400

    omg it works

---
+++ PythonReceiveData.py
@@ -0,0 +1,67 @@
+import serial
+import math
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import json
+
+def analogInputToDistance(input):
+  return 1.9252e+04 * (input ** -1.0776)
+
+arduinoComPort = "COM5"
+baudRate = 9600
+serialPort = serial.Serial(arduinoComPort, baudRate, timeout=1)
+
+data = []
+all_data = [[],[],[]]
+
+while True:
+  
+  lineOfData = serialPort.readline().decode().strip()
+
+  if len(lineOfData) > 0:
+    
+    if (lineOfData=="hello world"):
+      print("break")
+      break
+    
+    # print(lineOfData)
+  
+    if(lineOfData=="next" and len(data)==3):
+    
+      all_data[0].append(data[0])
+      all_data[1].append(data[1])
+      all_data[2].append(data[2])
+      
+      theta = math.radians((data[0]) - 45)
+      phi = math.radians((data[1]) + 60)
+      rho = analogInputToDistance((data[2]))
+      
+      # print((data[0]) - 45, (data[1]) + 60, rho)
+      
+      x = rho * math.cos(theta) * math.sin(phi)
+      y = rho * math.sin(theta) * math.sin(phi)
+      z = rho * math.cos(phi)
+      
+      data = []
+      
+    elif (lineOfData!="next"):
+      data.append((int)(lineOfData))
+
+
+try:
+  with open("data.json", "w") as f:
+    json.dump(all_data, f)
+except Exception as e:
+  print(e)
+  
+
+fig = plt.figure()
+# ax = fig.add_subplot(projection='3d')
+ax = fig.add_subplot()
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+# ax.set_zlabel("z")
+
+ax.scatter(all_data[0], all_data[1], c=all_data[2], cmap="Greys", norm="linear")
+
+plt.show()
---
+++ meow/meow.ino
@@ -0,0 +1,82 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
+*/
+
+#include <Servo.h>
+
+Servo motor1; // upper motor
+Servo motor2; // lower motor
+// twelve Servo objects can be created on most boards
+
+int distanceSensor = A0;
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  Serial.begin(9600);
+  motor1.attach(9);  // attaches the servo on pin 9 to the Servo object
+  motor2.attach(10);
+}
+
+int averageSensorValues(){
+  int samples = 2;
+  int sum = 0;
+  for(int i=0; i<samples; i++){
+    delay(50);
+    sum += analogRead(distanceSensor);
+  }
+  return sum/samples;
+}
+
+void loop() {
+
+  motor1.write(45);
+  motor2.write(0);
+  delay(5000);
+
+  bool dir = 0; // true for going up, false for going down
+  int angle1 = 45;
+  int angle2 = 0;
+
+  int angleInterval = 1;
+  int delay = 4; // in milliseconds
+
+  for (; angle1<=135; angle1+=angleInterval){
+    // Serial.println(phi);
+    motor1.write(angle1);
+    delay(4);
+    if(dir){
+      for (; angle2<60; angle2+=angleInterval){
+        motor2.write(angle2);
+        delay(4);
+        Serial.println(angle1);
+        // Serial.print(" ");
+        Serial.println(angle2);
+        // Serial.print(" ");
+        Serial.println(averageSensorValues());
+        Serial.println("next");
+      }
+    } else {
+      for (; angle2>0; angle2-=angleInterval){
+        motor2.write(angle2);
+        delay(4);
+        Serial.println(angle1);
+        // Serial.print(" ");
+        Serial.println(angle2);
+        // Serial.print(" ");
+        Serial.println(averageSensorValues());
+        Serial.println("next");
+      }
+    }
+    dir = !dir;
+  }
+
+  Serial.println("hello world");
+
+  while(1){ delay(1000); }
+}
